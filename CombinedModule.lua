6f62607c3d1fc590291313420ae3ea099d541807
habibrodriguez7-art <habibrodriguez7@gmail.com>
2026-01-03 00:54:16 +0800

    Update CombinedModule.lua

--- CombinedModule.lua
+++ CombinedModule.lua
@@ -7443,4 +7443,23 @@ end)
 return WalkOnWater
 end)()
 
+CombinedModules.WalkOnWater = (function()
+local Notification = {}
+
+function Notification.Send(title, text, duration)
+    duration = duration or 4
+
+    -- Gunakan pcall supaya tidak error di Delta / exploit lain
+    pcall(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = title,
+            Text = text,
+            Duration = duration
+        })
+    end)
+end
+
+return Notification
+end)()
+
 return CombinedModules
